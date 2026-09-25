03572445d452c510511191869aca9624088d9c73
EEA <58366551+B-a93@users.noreply.github.com>
2026-09-25 20:49:22 +0400

    Serve sitemap and robots content types

--- apps/school-web/server.mjs
+++ apps/school-web/server.mjs
@@ -22,7 +22,9 @@ const types = {
   ".css": "text/css; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
   ".svg": "image/svg+xml; charset=utf-8",
+  ".txt": "text/plain; charset=utf-8",
   ".webmanifest": "application/manifest+json; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
 };
 const json = (res, status, data) => {
   res.writeHead(status, {
